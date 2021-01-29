c3a026593b7f3041205e3ee0df9239ec565e82b2
SemihDurmus <semihdurmus@yahoo.com>
2021-01-29 09:29:28 +0100

    Home Page + loading + Cards

---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+
+const Contact = () => {
+  return (
+    <div>
+      <Navbar />
+      <p>Contact</p>
+    </div>
+  );
+};
+
+export default Contact;
--- src/router/Router.js
+++ src/router/Router.js
@@ -3,6 +3,8 @@ import { ContextProvider } from "../context/Context";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Landing from "../pages/Landing";
 import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
 
 export default function Router() {
   return (
@@ -13,9 +15,11 @@ export default function Router() {
           <Switch>
             <Route path="/" component={Landing} exact />
             <Route path="/home" component={Home} exact />
+            <Route path="/about" component={About} exact />
+            <Route path="/contact" component={Contact} exact />
             {/* 
-          <Route path="/detail/:slug" component={PostDetail} exact />
-        <Route path="/profile" component={ProfilePage} exact /> */}
+            <Route path="/detail/:slug" component={PostDetail} exact />
+            <Route path="/profile" component={ProfilePage} exact /> */}
             {/* <Route path="/create" component={PostPage} exact/>
               <Route path="/edit/:slug" component={CustomPostPage} exact/> */}
           </Switch>
